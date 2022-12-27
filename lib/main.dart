fb9368f07a5ad62e3cf8497949cb7e35e9883348
Murari <murarithapa4@gmail.com>
2022-12-27 10:01:04 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,67 @@
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:fluttersample/Student.dart';
-// import 'package:fluttersample/car.dart';
-// import 'package:fluttersample/models/bank.dart';
-
-// void bank(){
-// Bank;
-//
-// }
-//
-// void car(){
-//   Car;
 //
 //
-// }
 //
-// void student(){
-  //Student;//referance variable
- // Final student1.name = 'rahul';
- //  student.rollnumber =1;
- //
- //  print('name ${student1.name} and rollnumber ${student.rollnumber}';
+//
+//
+//
+//
+//
+//
+//
+//
+// // import 'package:fluttersample/Student.dart';
+// // import 'package:fluttersample/car.dart';
+// // import 'package:fluttersample/models/bank.dart';
+//
+// // void bank(){
+// // Bank;
+// //
+// // }
+// //
+// // void car(){
+// //   Car;
+// //
+// //
+// // }
+// //
+// // void student(){
+//   //Student;//referance variable
+//  // Final student1.name = 'rahul';
+//  //  student.rollnumber =1;
+//  //
+//  //  print('name ${student1.name} and rollnumber ${student.rollnumber}';
+//
+//
+// // }
+// // import 'package:fluttersample/models/sample.dart';
+//
+// /*
+// late required? ?? !
+//  */
+// void main(){
+//
+//   String? image;
+//   print(image);
+//
+// }A
+//
+// // void Animal(){
+// //   final dog1 = DOg();
+// //   dog1.sleep();;
+// //   dog1.breath();
+// //   dog1.eat();
+// //   final cat1 = Cat();
+// //   cat1.eat();
+// //   cat1.sleep();
+// //
+// //
+// //
+// // }
+//
+//
+import 'package:flutter/material.dart';
 
-
-// }
-// import 'package:fluttersample/models/sample.dart';
-
-/*
-late required? ?? !
- */
 void main(){
 
-  String? image;
-  print(image);
-
 }
-
-// void Animal(){
-//   final dog1 = DOg();
-//   dog1.sleep();;
-//   dog1.breath();
-//   dog1.eat();
-//   final cat1 = Cat();
-//   cat1.eat();
-//   cat1.sleep();
-//
-//
-//
-// }
-
-
+class Home extends StatelessWidget
